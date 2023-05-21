db29b8a703af0047b8daee9769074229b9c9c7b7
SalmaML <104851211+SalmaML@users.noreply.github.com>
2023-05-20 22:37:17 -0300

    Update Final_project_Week1.R

--- Final_project_Week1.R
+++ Final_project_Week1.R
@@ -1,5 +1,10 @@
 #clear all variables
 rm(list = ls())
+source("Plot1.R")
+source("Plot2.R")
+source("Plot3.R")
+source("Plot4.R")
+
 
 #load the necessary library
 # Functions t plot the result
@@ -7,55 +12,6 @@ rm(list = ls())
 readkey <- function() {
   line <- readline(prompt="Press [enter] to continue")
 }
-
-Plot1 <- function(df1) {
-  print( "Ploting Plot1...")
-  png('Plot1.png', width = 480, height = 480)
-  
-  par( mfrow = c(1,1), mar=c(4,4,2,1))
-  hist(as.double( df1$Global_active_power ), col='red', main='Global Active Power', xlab='Global Active Power (kilowatts)')
-
-  dev.off ();
-  }
-Plot2 <- function(df1) {
-  print( "Ploting Plot2...")
-  png('Plot2.png', width = 480, height = 480)
-  
-  par( mfrow = c(1,1), mar=c(4,4,2,1))
-    plot(df1$DayTime, df1$Global_active_power, type='l', ylab='Global Active Power (kilowatts)', xlab ='')
-    dev.off ();
-    
-  }
-Plot3 <- function(df1) {
-  print( "Ploting Plot3...")
-  png('Plot3.png', width = 480, height = 480)
-  
-  par( mfrow = c(1,1), mar=c(4,4,2,1))
-    plot(df1$DayTime, df1$Sub_metering_1 , type='l', ylab='Energy Sub metering', xlab ='')
-  lines(df1$DayTime, df1$Sub_metering_2, col='red', type='l')
-  lines(df1$DayTime, df1$Sub_metering_3, col='blue', type='l')
-  legend("topright", lty=1, col=c("black", "red", "blue"), c( 'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
-  dev.off ();
-  
-  }
-
-Plot4 <- function(df1) {
-  print( "Ploting Plot4..")
-  png('Plot4.png', width = 480, height = 480)
-  
-  par( mfrow = c(2,2), mar=c(4,4,2,1))
-  plot(df1$DayTime, df1$Global_active_power  , type='l', ylab='Global Active Ppower', xlab ='')
-  plot(df1$DayTime, df1$Voltage  , type='l', ylab='Voltage', xlab ='datetime')
-  
-  plot(df1$DayTime, df1$Sub_metering_1 , type='l', ylab='Energy Sub metering', xlab ='')
-  lines(df1$DayTime, df1$Sub_metering_2, col='red', type='l')
-  lines(df1$DayTime, df1$Sub_metering_3, col='blue', type='l')
-  legend("topright", lty=1, col=c("black", "red", "blue"), c( 'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
-  
-  plot(df1$DayTime, df1$Global_reactive_power   , type='l', ylab='Global Reactive Ppower ', xlab ='datetime')
-  dev.off ();
-  
-  }
 
 print( "Load file...")
 
@@ -84,7 +40,6 @@ readkey()
 # Plot-2: Global Active Power
 df1$time_temp <- paste(df1$Date, df1$Time)
 df1$DayTime <- strptime(df1$time_temp, format = "%Y-%m-%d %H:%M:%S")
-
 Plot2(df1)
 readkey()
 
@@ -93,4 +48,5 @@ Plot3(df1)
 readkey()
 
 # Plot-4: Sub_metering_1, 2, 3
+
 Plot4(df1)
